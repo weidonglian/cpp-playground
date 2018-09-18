4ec6fbdbbe4163b7a02c1e14b1d5767df836baeb
Weidong Lian <weidong.lian@gmail.com>
2018-09-18 22:25:31 +0200

    time duration clock play around

---
+++ time_duration_clock.cpp
@@ -0,0 +1,22 @@
+#include <chrono>
+#include <iostream>
+
+int main() {
+    using namespace std::chrono_literals;
+    using namespace std::chrono;
+    const auto s = 200ms;
+    const auto t = 500ms;
+    const auto d_st = s - t;
+    const auto d_ts = t - s;
+    const seconds d_ts_sec = duration_cast<seconds>(d_ts);
+    const microseconds d_ts_microsec = d_ts; // no need to duration_cast anymore
+
+    std::cout << "d_st=" << d_st.count() << std::endl; // Negative
+    std::cout << "d_ts=" << d_ts.count() << std::endl;
+    std::cout << "d_ts_sec=" << d_ts_sec.count() << std::endl;
+
+    const auto ss = steady_clock::now();    
+    const auto tt = steady_clock::now();
+    const seconds d_tt_ss_seconds = duration_cast<seconds>(tt-ss);
+    return 0;
+}
