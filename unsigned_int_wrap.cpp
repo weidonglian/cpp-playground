5cf4352e8a934dc279f73c1b62b480405bd207a7
Weidong Lian <weidong.lian@gmail.com>
2018-09-20 13:21:23 +0200

    unsigned int wrap example

--- unsigned_int_wrap.cpp
+++ unsigned_int_wrap.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-
+#include <limits>
 
 int main() {
     using namespace std;
-    int64_t big_int64 = std::numeric_limits<int32_t>::max() + 400;
-    uint32_t big_int64_mod = static_cast<uint32_t>(big_int64);
-    cout << big_int64_mod << endl;
+    int64_t big_int64 = std::numeric_limits<int32_t>::max();
+    cout << big_int64 << endl;
+    big_int64 += 400;
+    cout << big_int64 << endl;
+    uint32_t big_int64_mod1 = static_cast<uint32_t>(big_int64%std::numeric_limits<int32_t>::max());
+    cout << big_int64_mod1 << endl;
+    
+    uint32_t big_int64_mod2 = static_cast<uint32_t>(big_int64);
+    cout << big_int64_mod2 << endl;
+    
     return 0;
 }
