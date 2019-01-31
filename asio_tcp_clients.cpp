5952f67e8e90ebc8f7078a7da571ee6cebdd7a4e
Weidong Lian <weidong.lian@gmail.com>
2019-01-31 21:48:47 +0100

    asio tcp client

--- asio_tcp_clients.cpp
+++ asio_tcp_clients.cpp
@@ -8,13 +8,25 @@ int main(int argc, char ** argv) {
     io_context ctx;
     ip::tcp::resolver resolver(ctx);
     ip::tcp::resolver::query query("www.boost.org", "https");
-    ip::tcp::resolver::iterator iter = resolver.resolve(query);
-    ip::tcp::resolver::iterator end; // End marker.
-    
-    while (iter != end) {
-        ip::tcp::endpoint endpoint = *iter++;
-        std::cout << endpoint << std::endl;
+
+    // End points    
+    {
+        ip::tcp::resolver::iterator iter = resolver.resolve(query);
+        ip::tcp::resolver::iterator end; // End marker.
+        
+        while (iter != end) {
+            ip::tcp::endpoint endpoint = *iter++;
+            std::cout << endpoint << std::endl;
+        }
     }
 
+    ip::tcp::socket sokt(ctx);
+    async_connect(sokt, resolver.resolve(query), [](const error_code& er, const ip::tcp::endpoint& endpoint) {
+        if (!er) {
+
+        } else {
+            std::cout << "async_connect error:" << er.message() << std::endl;
+        }
+    });
     return 0;
 }
