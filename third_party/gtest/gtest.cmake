efa22c7bd5f4a8857516a3dd706ca612e371b7dd
Weidong Lian <weidong.lian@gmail.com>
2019-03-22 17:54:43 +0100

    Add gtest play case support

--- third_party/gtest/gtest.cmake
+++ third_party/gtest/gtest.cmake
@@ -1,34 +1,30 @@
-if(ENABLE_GTEST)
-  # Package/Project DIR.
-  set(GTEST_DIR ${CMAKE_CURRENT_LIST_DIR}/gtest)
-  
-  message(STATUS "Using GTest - ${GTEST_DIR}")
-  
-  # Include DIR List, Relative
-  set(GTEST_INC
-  ${GTEST_DIR}/include
-  ${GTEST_DIR}
-  )
-  
-  # SRC File List, Relative
-  set(GTEST_SRC
-  ${GTEST_DIR}/src/gtest_main.cc
-  ${GTEST_DIR}/src/gtest.cc
-  ${GTEST_DIR}/src/gtest-death-test.cc
-  ${GTEST_DIR}/src/gtest-filepath.cc
-  ${GTEST_DIR}/src/gtest-port.cc
-  ${GTEST_DIR}/src/gtest-printers.cc
-  ${GTEST_DIR}/src/gtest-test-part.cc
-  ${GTEST_DIR}/src/gtest-typed-test.cc
-  )
-  
-  # Generate HDR List.
-  foreach(DIR ${GTEST_INC})
-    file(GLOB_RECURSE GTEST_HDR ${GTEST_INC}/*.h)
-    file(GLOB_RECURSE GTEST_HDR ${GTEST_INC}/*.hpp)
-  endforeach()
-  
-  add_definitions(-DBIOSOL_HAVE_GTEST)
-else(ENABLE_GTEST)
-  message(FATAL_ERROR "Gtest is not on.")
-endif(ENABLE_GTEST)
+# Package/Project DIR.
+set(GTEST_DIR ${CMAKE_CURRENT_LIST_DIR}/gtest)
+
+message(STATUS "Using GTest - ${GTEST_DIR}")
+
+# Include DIR List, Relative
+set(GTEST_INC
+${GTEST_DIR}/include
+${GTEST_DIR}
+)
+
+# SRC File List, Relative
+set(GTEST_SRC
+${GTEST_DIR}/src/gtest_main.cc
+${GTEST_DIR}/src/gtest.cc
+${GTEST_DIR}/src/gtest-death-test.cc
+${GTEST_DIR}/src/gtest-filepath.cc
+${GTEST_DIR}/src/gtest-port.cc
+${GTEST_DIR}/src/gtest-printers.cc
+${GTEST_DIR}/src/gtest-test-part.cc
+${GTEST_DIR}/src/gtest-typed-test.cc
+)
+
+# Generate HDR List.
+foreach(DIR ${GTEST_INC})
+file(GLOB_RECURSE GTEST_HDR ${GTEST_INC}/*.h)
+file(GLOB_RECURSE GTEST_HDR ${GTEST_INC}/*.hpp)
+endforeach()
+
+add_definitions(-DCPP_PLAYGROUND_HAVE_GTEST)
