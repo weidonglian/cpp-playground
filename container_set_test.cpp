9090a9963e378318c6cb9492c23d6d969bc39038
Weidong Lian <weidong.lian@gmail.com>
2019-03-22 18:44:13 +0100

    Add new test case

--- container_set_test.cpp
+++ container_set_test.cpp
@@ -84,3 +84,56 @@ TEST(ContainerSet, mutable_key) {
 
   EXPECT_EQ(0, timer_set.size());
 }
+
+TEST(ContainerSet, mutable_key_large) {
+  std::vector<DeadlineTimer> timers;
+  timers.reserve(1000);
+  for (int i = 0; i < 1000; ++i) {
+    timers.emplace_back(i, i);
+  };
+  
+  DeadlineTimerSet timer_set;
+
+  for (auto& t : timers) {
+    timer_set.insert(&t);
+  }
+  const auto nb_timers = timers.size();
+  EXPECT_EQ(nb_timers, timer_set.size());
+
+  for (auto& t : timers) {
+    timer_set.insert(&t);
+    EXPECT_EQ(nb_timers, timer_set.size());
+  }
+
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_int_distribution<> dis(-10010, 10000);
+
+  for (auto& t : timers) {
+    t.expired_time_ = dis(gen);
+    timer_set.insert(&t);
+    EXPECT_EQ(nb_timers, timer_set.size());
+  }
+
+  int i = 1;
+  for (auto& t : timers) {
+    t.expired_time_ += dis(gen);
+    timer_set.erase(&t);
+    EXPECT_EQ(nb_timers-i++, timer_set.size());
+  }
+
+  EXPECT_EQ(0, timer_set.size());
+
+  int j = 1;
+  for (auto& t : timers) {
+    t.expired_time_ += dis(gen);
+    timer_set.insert(&t);
+    EXPECT_EQ(j++, timer_set.size());
+  }
+
+  for (auto& t : timers) {
+    timer_set.erase(&t);
+  }
+
+  EXPECT_EQ(0, timer_set.size());
+}
