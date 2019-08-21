fb537f8a366a6214d249027572fa1690715d8587
Weidong Lian <weidong.lian@gmail.com>
2019-08-22 00:37:39 +0200

    array minimum bribes

---
+++ src/data_structures_algorithms/array_minimum_bribes.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+#include <optional>
+#include <algorithm>
+#include "cpptest.hpp"
+
+
+using namespace std;
+
+optional<int> array_minimum_bribes(vector<int> q) {
+    const auto v_max = q.size();
+    int cnt = 0;
+    for (int v = v_max; v > 0; --v) {
+        for (int i = 0; i < 3; ++i) {
+            // v should be in the position v-1
+            const int should_pos = v-1;
+            if (q[should_pos - i] == v) {
+                // move to right i th.
+                for (int j = 0; j < i; ++j) {
+                    std::swap(q[should_pos - i + j], q[should_pos - i + j + 1]);
+                }
+                break;
+            } else if (i == 2) {
+                return nullopt;
+            } else {
+                ++cnt;
+            }
+        }
+    }
+    return cnt;
+}
+
+TEST(ArraySuite, array_minimum_bribes) {
+    auto r1 = array_minimum_bribes({2, 1, 5, 3, 4});
+    EXPECT_TRUE(r1.has_value());
+    EXPECT_EQ(3, r1.value());
+
+    auto r2 = array_minimum_bribes({2, 5, 1, 3, 4});
+    EXPECT_FALSE(r2.has_value());
+}
