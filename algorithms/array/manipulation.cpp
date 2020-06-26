6812ce1363565556f0ade91c7554f58787e1c920
Weidong Lian <weidong.lian@gmail.com>
2020-06-26 23:07:17 +0200

    refactoring the structure

--- algorithms/array/manipulation.cpp
+++ algorithms/array/manipulation.cpp
@@ -8,66 +8,67 @@
 
 using namespace std;
 
-long array_manipulation(int n, const vector<array<int, 3>> &queries) {
-    vector<int> deltas(n);
-    for (const auto &query : queries) {
-        const auto left = query[0] - 1;
-        const auto right = query[1] - 1;
-        const auto val = query[2];
-        deltas[left] += val;
-        if (right + 1 < n)
-            deltas[right + 1] -= val;
-    }
+long array_manipulation(int n, const vector<array<int, 3>>& queries) {
+  vector<int> deltas(n);
+  for (const auto& query : queries) {
+    const auto left = query[0] - 1;
+    const auto right = query[1] - 1;
+    const auto val = query[2];
+    deltas[left] += val;
+    if (right + 1 < n)
+      deltas[right + 1] -= val;
+  }
 
-    long max = 0;
-    long current = 0;
-    for (auto d : deltas) {
-        current += d;
-        if (current > max) {
-            max = current;
-        }
+  long max = 0;
+  long current = 0;
+  for (auto d : deltas) {
+    current += d;
+    if (current > max) {
+      max = current;
     }
-    return max;
+  }
+  return max;
 }
 
-std::string test_array_manipulation(const std::string &input) {
-    istringstream iss(input);
-    int n = 0, m = 0;
-    iss >> n >> m;
-    vector<array<int, 3>> queries(m);
-    for (auto &query : queries) {
-        iss >> query[0] >> query[1] >> query[2];
-    }
-    const auto result = array_manipulation(n, queries);
-    return to_string(result);
+std::string test_array_manipulation(const std::string& input) {
+  istringstream iss(input);
+  int n = 0, m = 0;
+  iss >> n >> m;
+  vector<array<int, 3>> queries(m);
+  for (auto& query : queries) {
+    iss >> query[0] >> query[1] >> query[2];
+  }
+  const auto result = array_manipulation(n, queries);
+  return to_string(result);
 }
 
 TEST_CASE("array_manipulation", "[array]") {
-    const array<test_input_output, 3> cases = {
-        test_input_output{
-            R"(5 3
+  using test_input_output = std::tuple<std::string, std::string>;
+  const array<test_input_output, 3> cases = {
+    test_input_output{
+      R"(5 3
 1 2 100
 2 5 100
 3 4 100)",
-            R"(200)"},
+      R"(200)"},
 
-        test_input_output{
-            R"(10 3
+    test_input_output{
+      R"(10 3
 1 5 3
 4 8 7
 6 9 1)",
-            R"(10)"},
+      R"(10)"},
 
-        test_input_output{
-            R"(10 4
+    test_input_output{
+      R"(10 4
 2 6 8
 3 5 7
 1 8 1
 5 9 15)",
-            R"(31)"},
-    };
+      R"(31)"},
+  };
 
-    for (const auto &e : cases) {
-        CHECK(test_array_manipulation(e.input) == e.output);
-    }
+  for (const auto& [input, expected_output] : cases) {
+    CHECK(test_array_manipulation(input) == expected_output);
+  }
 }
--- algorithms/tree/find_nearest_neighbour.cpp
+++ algorithms/tree/find_nearest_neighbour.cpp
@@ -1,5 +1,3 @@
-#include "algorithms.hpp"
-#include "simple_svg.hpp"
 #include <array>
 #include <cmath>
 #include <cstddef>
@@ -9,8 +7,11 @@
 #include <map>
 #include <memory>
 #include <optional>
-#include <random>
 #include <vector>
+
+#include "algorithms.hpp"
+#include "random.hpp"
+#include "simple_svg.hpp"
 
 using namespace std;
 
@@ -20,188 +21,178 @@ constexpr int k_dims = 2;
 using point = array<int, k_dims>;
 
 // Mahatan distance
-int distance(const point &p1, const point &p2) {
-    int dist = 0;
-    for (int i = 0; i < k_dims; ++i)
-        dist += abs(p1[i] - p2[i]);
-    return dist;
+int distance(const point& p1, const point& p2) {
+  int dist = 0;
+  for (int i = 0; i < k_dims; ++i)
+    dist += abs(p1[i] - p2[i]);
+  return dist;
 }
 
 struct kd_node {
-    kd_node *lo; // left/below, lower
-    kd_node *hi; // right/above, higher
-    point pt;    // the node's point if no children node, i.e. leaf
+  kd_node* lo; // left/below, lower
+  kd_node* hi; // right/above, higher
+  point pt;    // the node's point if no children node, i.e. leaf
 };
 
-kd_node *kd_new(point pt) {
-    return new kd_node{nullptr, nullptr, pt};
+kd_node* kd_new(point pt) {
+  return new kd_node{nullptr, nullptr, pt};
 }
 
-void kd_delete(kd_node *root) {
-    if (!root) {
-        return;
-    }
+void kd_delete(kd_node* root) {
+  if (!root) {
+    return;
+  }
 
-    kd_delete(root->lo);
-    kd_delete(root->hi);
-    delete root;
+  kd_delete(root->lo);
+  kd_delete(root->hi);
+  delete root;
 }
 
-kd_node *kd_insert(kd_node *root, point pt, int depth = 0) {
-    if (!root) {
-        return kd_new(pt); // tree is empty and return a new root
-    }
+kd_node* kd_insert(kd_node* root, point pt, int depth = 0) {
+  if (!root) {
+    return kd_new(pt); // tree is empty and return a new root
+  }
 
-    const int dim = depth % k_dims;
-    if (pt[dim] < root->pt[dim]) {
-        root->lo = kd_insert(root->lo, pt, depth + 1);
-    } else {
-        root->hi = kd_insert(root->hi, pt, depth + 1);
-    }
-    return root;
+  const int dim = depth % k_dims;
+  if (pt[dim] < root->pt[dim]) {
+    root->lo = kd_insert(root->lo, pt, depth + 1);
+  } else {
+    root->hi = kd_insert(root->hi, pt, depth + 1);
+  }
+  return root;
 }
 
 template <class InputIterator>
-kd_node *kd_build(InputIterator first, InputIterator last) {
-    kd_node *root = nullptr;
-    while (first != last) {
-        root = kd_insert(root, *first);
-        ++first;
-    }
-    return root;
+kd_node* kd_build(InputIterator first, InputIterator last) {
+  kd_node* root = nullptr;
+  while (first != last) {
+    root = kd_insert(root, *first);
+    ++first;
+  }
+  return root;
 }
 
 // Find the node that the given point should be inserted
-const kd_node *kd_find_insert_position(const kd_node *root, point pt,
-                                       int depth = 0) {
-    if (!root)
-        return root;
-    const int dim = depth % k_dims;
-    if (pt[dim] < root->pt[dim]) {
-        return root->lo ? kd_find_insert_position(root->lo, pt, depth + 1)
-                        : root;
-    } else {
-        return root->hi ? kd_find_insert_position(root->hi, pt, depth + 1)
-                        : root;
-    }
+const kd_node* kd_find_insert_position(const kd_node* root, point pt, int depth = 0) {
+  if (!root)
+    return root;
+  const int dim = depth % k_dims;
+  if (pt[dim] < root->pt[dim]) {
+    return root->lo ? kd_find_insert_position(root->lo, pt, depth + 1) : root;
+  } else {
+    return root->hi ? kd_find_insert_position(root->hi, pt, depth + 1) : root;
+  }
 }
 
 // Find the nearest neightbour with condtion <= min_dist, otherwise return
 // nullptr
-const kd_node *kd_find_nearest(const kd_node *root, point pt_query,
-                               int min_dist, int depth = 0) {
-    // Current node is empty, then return it.
-    if (!root)
-        return nullptr;
+const kd_node* kd_find_nearest(const kd_node* root, point pt_query, int min_dist, int depth = 0) {
+  // Current node is empty, then return it.
+  if (!root)
+    return nullptr;
 
-    const kd_node *best = nullptr;
-    // Current node to update the min_dist
-    {
-        const int dst = distance(pt_query, root->pt);
-        if (dst < min_dist) {
-            min_dist = dst;
-            best = root;
-        }
+  const kd_node* best = nullptr;
+  // Current node to update the min_dist
+  {
+    const int dst = distance(pt_query, root->pt);
+    if (dst < min_dist) {
+      min_dist = dst;
+      best = root;
     }
+  }
 
-    // determine which branch to search
-    const int dim = depth % k_dims;
-    // distance in the corresponding dimension
-    const int signed_dst = pt_query[dim] - root->pt[dim]; // signed distance
-    bool should_search_lo = false;
-    bool should_search_hi = false;
-    if (signed_dst < -min_dist) {
-        // the query point on the far left side of the dim axis. search the lo
-        // branch
-        should_search_lo = true;
-    } else if (signed_dst > min_dist) {
-        // the query point on the far right side of the dim axis. search only
-        // the hi branch
-        should_search_hi = true;
-    } else {
-        // Too close the boundary, We need to search two branches, lo and hi.
-        should_search_lo = true;
-        should_search_hi = true;
-    }
+  // determine which branch to search
+  const int dim = depth % k_dims;
+  // distance in the corresponding dimension
+  const int signed_dst = pt_query[dim] - root->pt[dim]; // signed distance
+  bool should_search_lo = false;
+  bool should_search_hi = false;
+  if (signed_dst < -min_dist) {
+    // the query point on the far left side of the dim axis. search the lo
+    // branch
+    should_search_lo = true;
+  } else if (signed_dst > min_dist) {
+    // the query point on the far right side of the dim axis. search only
+    // the hi branch
+    should_search_hi = true;
+  } else {
+    // Too close the boundary, We need to search two branches, lo and hi.
+    should_search_lo = true;
+    should_search_hi = true;
+  }
 
-    if (should_search_lo && root->lo) {
-        const kd_node *best_lo =
-            kd_find_nearest(root->lo, pt_query, min_dist, depth + 1);
-        if (best_lo) { // exist must be smaller
-            assert(distance(pt_query, best_lo->pt) < min_dist);
-            min_dist = distance(pt_query, best_lo->pt);
-            best = best_lo;
-        }
+  if (should_search_lo && root->lo) {
+    const kd_node* best_lo = kd_find_nearest(root->lo, pt_query, min_dist, depth + 1);
+    if (best_lo) { // exist must be smaller
+      assert(distance(pt_query, best_lo->pt) < min_dist);
+      min_dist = distance(pt_query, best_lo->pt);
+      best = best_lo;
     }
+  }
 
-    if (should_search_hi && root->hi) {
-        const kd_node *best_hi =
-            kd_find_nearest(root->hi, pt_query, min_dist, depth + 1);
-        if (best_hi) {
-            assert(distance(pt_query, best_hi->pt) < min_dist);
-            min_dist = distance(pt_query, best_hi->pt);
-            best = best_hi;
-        }
+  if (should_search_hi && root->hi) {
+    const kd_node* best_hi = kd_find_nearest(root->hi, pt_query, min_dist, depth + 1);
+    if (best_hi) {
+      assert(distance(pt_query, best_hi->pt) < min_dist);
+      min_dist = distance(pt_query, best_hi->pt);
+      best = best_hi;
     }
-    // both lo and hi branches do not find anything, then return root.
-    return best;
+  }
+  // both lo and hi branches do not find anything, then return root.
+  return best;
 }
 
-optional<point> nns_kd_tree_search(const vector<point> &points,
-                                   point pt_query) {
-    kd_node *root = kd_build(begin(points), end(points));
+optional<point> nns_kd_tree_search(const vector<point>& points, point pt_query) {
+  kd_node* root = kd_build(begin(points), end(points));
 
-    int min_dist = numeric_limits<int>::max();
-    const kd_node *best_node = nullptr;
-    {
-        // Try to guess what is possible min distance
-        const kd_node *init_guess = kd_find_insert_position(root, pt_query);
-        if (init_guess) {
-            min_dist = distance(pt_query, init_guess->pt);
-            best_node = init_guess;
-        }
+  int min_dist = numeric_limits<int>::max();
+  const kd_node* best_node = nullptr;
+  {
+    // Try to guess what is possible min distance
+    const kd_node* init_guess = kd_find_insert_position(root, pt_query);
+    if (init_guess) {
+      min_dist = distance(pt_query, init_guess->pt);
+      best_node = init_guess;
     }
+  }
 
-    const kd_node *nearest = kd_find_nearest(root, pt_query, min_dist);
-    if (nearest) {
-        min_dist = distance(pt_query, nearest->pt);
-        best_node = nearest;
-    }
+  const kd_node* nearest = kd_find_nearest(root, pt_query, min_dist);
+  if (nearest) {
+    min_dist = distance(pt_query, nearest->pt);
+    best_node = nearest;
+  }
 
-    optional<point> ret = nullopt;
-    if (best_node)
-        ret = make_optional(best_node->pt);
+  optional<point> ret = nullopt;
+  if (best_node)
+    ret = make_optional(best_node->pt);
 
-    kd_delete(root);
+  kd_delete(root);
 
-    return ret;
+  return ret;
 }
 
-optional<point> nns_linear_search(const vector<point> &points, point pt_query) {
-    optional<point> result;
-    int min_dst = 0;
-    const int sz_total = static_cast<int>(points.size());
-    for (int idx = 0; idx < sz_total; ++idx) {
-        const int dst = distance(points[idx], pt_query);
-        if (!result || dst < min_dst) {
-            result = points[idx];
-            min_dst = dst;
-        }
+optional<point> nns_linear_search(const vector<point>& points, point pt_query) {
+  optional<point> result;
+  int min_dst = 0;
+  const int sz_total = static_cast<int>(points.size());
+  for (int idx = 0; idx < sz_total; ++idx) {
+    const int dst = distance(points[idx], pt_query);
+    if (!result || dst < min_dst) {
+      result = points[idx];
+      min_dst = dst;
     }
-    return result;
+  }
+  return result;
 }
 
 enum class find_policy { defaut, linear_search, kd_tree };
 
-optional<point> find_nearest_neighbour(const vector<point> &points,
-                                       point pt_query, find_policy policy) {
-    using fn_search =
-        std::function<optional<point>(const vector<point> &, point)>;
-    static const std::map<find_policy, fn_search> algors = {
-        {find_policy::linear_search, nns_linear_search},
-        {find_policy::kd_tree, nns_kd_tree_search},
-        {find_policy::defaut, nns_kd_tree_search}};
-    return algors.at(policy)(points, pt_query);
+optional<point> find_nearest_neighbour(const vector<point>& points, point pt_query, find_policy policy) {
+  using fn_search = std::function<optional<point>(const vector<point>&, point)>;
+  static const std::map<find_policy, fn_search> algors = {{find_policy::linear_search, nns_linear_search},
+                                                          {find_policy::kd_tree, nns_kd_tree_search},
+                                                          {find_policy::defaut, nns_kd_tree_search}};
+  return algors.at(policy)(points, pt_query);
 }
 
 } // namespace nns
@@ -211,112 +202,97 @@ namespace details {
 using namespace nns;
 
 vector<point> generate_random_cloud_points(size_t sz) {
-    vector<int> xx = generate_random_number(sz);
-    vector<int> yy = generate_random_number(sz);
-    vector<point> points;
-    points.reserve(sz);
-    int cnt = 0;
-    while (cnt++ < sz) {
-        points.push_back({xx[cnt], yy[cnt]});
-    }
-    return points;
+  vector<int> xx = generate_random_number(sz);
+  vector<int> yy = generate_random_number(sz);
+  vector<point> points;
+  points.reserve(sz);
+  int cnt = 0;
+  while (cnt++ < sz) {
+    points.push_back({xx[cnt], yy[cnt]});
+  }
+  return points;
 }
 
-void dump_svg(const string &file_name, const vector<point> &points, point test,
-              point q_linear, point q_kd_tree) {
-    using namespace svg;
-    Dimensions dimensions(-1000, 1000);
-    Document doc(file_name, Layout(dimensions, Layout::BottomLeft));
+void dump_svg(const string& file_name, const vector<point>& points, point test, point q_linear, point q_kd_tree) {
+  using namespace svg;
+  Dimensions dimensions(-1000, 1000);
+  Document doc(file_name, Layout(dimensions, Layout::BottomLeft));
 
-    // Red image border.
-    // Polygon border(Stroke(1, Color::Red));
-    // border << Point(0, 0) << Point(dimensions.width, 0)
-    //       << Point(dimensions.width, dimensions.height)
-    //       << Point(0, dimensions.height);
-    // doc << border;
+  // Red image border.
+  // Polygon border(Stroke(1, Color::Red));
+  // border << Point(0, 0) << Point(dimensions.width, 0)
+  //       << Point(dimensions.width, dimensions.height)
+  //       << Point(0, dimensions.height);
+  // doc << border;
 
-    for (const auto &p : points) {
-        doc << Circle(Point(p[0], p[1]), 8, Fill(Color(0, 255, 0)),
-                      Stroke(1, Color(0, 255, 0)));
-    }
+  for (const auto& p : points) {
+    doc << Circle(Point(p[0], p[1]), 8, Fill(Color(0, 255, 0)), Stroke(1, Color(0, 255, 0)));
+  }
 
-    doc << Circle(Point(test[0], test[1]), 16, Fill(Color(0, 0, 255)),
-                  Stroke(1, Color(0, 0, 200)));
+  doc << Circle(Point(test[0], test[1]), 16, Fill(Color(0, 0, 255)), Stroke(1, Color(0, 0, 200)));
 
-    doc << Circle(Point(q_linear[0], q_linear[1]), 20, Fill(Color(125, 0, 0)),
-                  Stroke(2, Color(0, 0, 0)));
+  doc << Circle(Point(q_linear[0], q_linear[1]), 20, Fill(Color(125, 0, 0)), Stroke(2, Color(0, 0, 0)));
 
-    doc << Circle(Point(q_kd_tree[0], q_kd_tree[1]), 20, Fill(Color(255, 0, 0)),
-                  Stroke(2, Color(0, 0, 0)));
+  doc << Circle(Point(q_kd_tree[0], q_kd_tree[1]), 20, Fill(Color(255, 0, 0)), Stroke(2, Color(0, 0, 0)));
 
-    doc.save();
+  doc.save();
 }
 } // namespace details
 
 TEST_CASE("find_nns_query_point_in_cluster", "[tree]") {
-    using namespace nns;
-    const vector<point> fixed_points = {{1, 2},    {5, 8},    {20, 4}, {2, 9},
-                                        {100, 40}, {43, -30}, {-10, 8}};
+  using namespace nns;
+  const vector<point> fixed_points = {{1, 2}, {5, 8}, {20, 4}, {2, 9}, {100, 40}, {43, -30}, {-10, 8}};
 
-    for (const auto &p : fixed_points) {
-        const auto q_linear =
-            find_nearest_neighbour(fixed_points, p, find_policy::linear_search);
-        REQUIRE(q_linear.has_value());
-        CHECK(q_linear.value() == p); // they should be the same
+  for (const auto& p : fixed_points) {
+    const auto q_linear = find_nearest_neighbour(fixed_points, p, find_policy::linear_search);
+    REQUIRE(q_linear.has_value());
+    CHECK(q_linear.value() == p); // they should be the same
 
-        const auto q_kd_tree =
-            find_nearest_neighbour(fixed_points, p, find_policy::kd_tree);
-        REQUIRE(q_kd_tree.has_value());
-        CHECK(q_kd_tree == q_linear); // find the existing point in cluster
-    }
+    const auto q_kd_tree = find_nearest_neighbour(fixed_points, p, find_policy::kd_tree);
+    REQUIRE(q_kd_tree.has_value());
+    CHECK(q_kd_tree == q_linear); // find the existing point in cluster
+  }
 }
 
 TEST_CASE("find_nns_query_point_in_cluster_random", "[tree]") {
-    using namespace nns;
-    const vector<point> points = details::generate_random_cloud_points(1000);
+  using namespace nns;
+  const vector<point> points = details::generate_random_cloud_points(1000);
 
-    int cnt = 0;
-    for (const auto &p : points) {
-        const auto q_linear =
-            find_nearest_neighbour(points, p, find_policy::linear_search);
-        REQUIRE(q_linear.has_value());
-        CHECK(q_linear.value() == p); // they should be the same
+  int cnt = 0;
+  for (const auto& p : points) {
+    const auto q_linear = find_nearest_neighbour(points, p, find_policy::linear_search);
+    REQUIRE(q_linear.has_value());
+    CHECK(q_linear.value() == p); // they should be the same
 
-        const auto q_kd_tree =
-            find_nearest_neighbour(points, p, find_policy::kd_tree);
-        REQUIRE(q_kd_tree.has_value());
-        // find existing point in cluster
-        INFO("with p=[" << p[0] << ", " << p[1] << "]");
-        CHECK(q_kd_tree.value() == q_linear.value());
-        if (q_kd_tree.value() != q_linear.value()) {
-            details::dump_svg("find_nearest_neighbour_random" +
-                                  to_string(cnt++) + ".svg",
-                              points, p, q_linear.value(), q_kd_tree.value());
-        }
+    const auto q_kd_tree = find_nearest_neighbour(points, p, find_policy::kd_tree);
+    REQUIRE(q_kd_tree.has_value());
+    // find existing point in cluster
+    INFO("with p=[" << p[0] << ", " << p[1] << "]");
+    CHECK(q_kd_tree.value() == q_linear.value());
+    if (q_kd_tree.value() != q_linear.value()) {
+      details::dump_svg("find_nearest_neighbour_random" + to_string(cnt++) + ".svg", points, p, q_linear.value(),
+                        q_kd_tree.value());
     }
+  }
 }
 
 TEST_CASE("find_nns_query_not_necessarily_from_cluster", "[tree]") {
-    using namespace nns;
-    const vector<point> points = details::generate_random_cloud_points(1000);
-    const vector<point> points_to_test =
-        details::generate_random_cloud_points(1000);
-    int cnt = 0;
-    for (const auto &p : points_to_test) {
-        const auto q_linear =
-            find_nearest_neighbour(points, p, find_policy::linear_search);
-        REQUIRE(q_linear.has_value());
+  using namespace nns;
+  const vector<point> points = details::generate_random_cloud_points(1000);
+  const vector<point> points_to_test = details::generate_random_cloud_points(1000);
+  int cnt = 0;
+  for (const auto& p : points_to_test) {
+    const auto q_linear = find_nearest_neighbour(points, p, find_policy::linear_search);
+    REQUIRE(q_linear.has_value());
 
-        const auto q_kd_tree =
-            find_nearest_neighbour(points, p, find_policy::kd_tree);
-        REQUIRE(q_kd_tree.has_value());
-        const int min_dist_linear = distance(q_linear.value(), p);
-        const int min_dist_kd_tree = distance(q_kd_tree.value(), p);
-        CHECK(min_dist_linear == min_dist_kd_tree);
-        if (min_dist_kd_tree != min_dist_linear) {
-            details::dump_svg("find_nearest_neighbour_random_point_" +
-                                  to_string(cnt++) + ".svg",
-                              points, p, q_linear.value(), q_kd_tree.value());
-        }
+    const auto q_kd_tree = find_nearest_neighbour(points, p, find_policy::kd_tree);
+    REQUIRE(q_kd_tree.has_value());
+    const int min_dist_linear = distance(q_linear.value(), p);
+    const int min_dist_kd_tree = distance(q_kd_tree.value(), p);
+    CHECK(min_dist_linear == min_dist_kd_tree);
+    if (min_dist_kd_tree != min_dist_linear) {
+      details::dump_svg("find_nearest_neighbour_random_point_" + to_string(cnt++) + ".svg", points, p, q_linear.value(),
+                        q_kd_tree.value());
     }
+  }
 }
