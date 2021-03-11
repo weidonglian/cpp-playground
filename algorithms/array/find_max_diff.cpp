2be3a6c9eeb07a02716e7f47e73d2719c5339dd2
Weidong Lian <weidong.lian@gmail.com>
2021-03-11 13:19:10 +0100

    add problem description

--- algorithms/array/find_max_diff.cpp
+++ algorithms/array/find_max_diff.cpp
@@ -1,96 +1,100 @@
-#include "algorithms.hpp"
 #include <algorithm>
 #include <functional>
 #include <vector>
 
+#include "algorithms.hpp"
+
 using namespace std;
 
 /// This will loop every possible combinations. Complexity: N*N
-int find_max_diff_brutal_force(const vector<int> &arr) {
-    int max_diff = 0;
-    for (int i = 0; i < arr.size(); i++) {
-        for (int j = i + 1; j < arr.size(); j++) {
-            auto diff = arr[j] - arr[i];
-            if (diff > max_diff) {
-                max_diff = diff;
-            }
-        }
+int find_max_diff_brutal_force(const vector<int>& arr) {
+  int max_diff = 0;
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = i + 1; j < arr.size(); j++) {
+      auto diff = arr[j] - arr[i];
+      if (diff > max_diff) {
+        max_diff = diff;
+      }
     }
-    return max_diff;
+  }
+  return max_diff;
 }
 
 /// Keep track of the min value from left and max_diff. Complexity: N.
-int find_max_diff_optimal(const vector<int> &arr) {
-    if (arr.empty())
-        return 0;
-    const int n = (int)arr.size();
-    int max_diff = 0;
-    int min_val = arr[0];
-    for (int i = 1; i < n; i++) {
-        int diff = arr[i] - min_val;
-        if (diff > max_diff) {
-            max_diff = diff;
-        }
-        if (arr[i] < min_val) {
-            min_val = arr[i];
-        }
+int find_max_diff_optimal(const vector<int>& arr) {
+  if (arr.empty())
+    return 0;
+  const int n = (int)arr.size();
+  int max_diff = 0;
+  int min_val = arr[0];
+  for (int i = 1; i < n; i++) {
+    int diff = arr[i] - min_val;
+    if (diff > max_diff) {
+      max_diff = diff;
     }
-    return max_diff;
+    if (arr[i] < min_val) {
+      min_val = arr[i];
+    }
+  }
+  return max_diff;
 }
 
-/// keep track of the adjacent dist and then find max sum of dist.
-int find_max_diff_relative_dist(const vector<int> &arr) {
-    const int n = (int)arr.size();
-    vector<int> diffs(n - 1);
-    for (int i = 0; i < diffs.size(); i++) {
-        diffs[i] = arr[i + 1] - arr[i];
+/// keep track of the adjacent dist and then find max sum of positive contributions.
+int find_max_diff_relative_dist(const vector<int>& arr) {
+  const int n = (int)arr.size();
+  vector<int> diffs(n - 1);
+  for (int i = 0; i < diffs.size(); i++) {
+    diffs[i] = arr[i + 1] - arr[i];
+  }
+  int max_diff = 0;
+  for (int i = 1; i < diffs.size(); i++) {
+    if (diffs[i - 1] > 0) {
+      diffs[i] += diffs[i - 1];
     }
-    int max_diff = 0;
-    for (int i = 1; i < diffs.size(); i++) {
-        if (diffs[i - 1] > 0) {
-            diffs[i] += diffs[i - 1];
-        }
-        if (diffs[i] > max_diff) {
-            max_diff = diffs[i];
-        }
+    if (diffs[i] > max_diff) {
+      max_diff = diffs[i];
     }
-    return max_diff;
+  }
+  return max_diff;
 }
 
 /// keep track of the adjacent dist and then find max sum of dist. space
 /// optimal.
-int find_max_diff_relative_dist_optimal(const vector<int> &arr) {
-    if (arr.size() < 2)
-        return 0;
-    int diff = 0;
-    int cur_sum = 0;
-    int max_sum = 0;
-    for (int i = 1; i < arr.size(); i++) {
-        diff = arr[i] - arr[i - 1];
-        if (cur_sum > 0) {
-            cur_sum += diff;
-        } else {
-            cur_sum = diff;
-        }
-        if (cur_sum > max_sum) {
-            max_sum = cur_sum;
-        }
+int find_max_diff_relative_dist_optimal(const vector<int>& arr) {
+  if (arr.size() < 2)
+    return 0;
+  int diff = 0;
+  int cur_sum = 0;
+  int max_sum = 0;
+  for (int i = 1; i < arr.size(); i++) {
+    diff = arr[i] - arr[i - 1];
+    if (cur_sum > 0) {
+      cur_sum += diff;
+    } else {
+      cur_sum = diff;
     }
-    return max_sum;
+    if (cur_sum > max_sum) {
+      max_sum = cur_sum;
+    }
+  }
+  return max_sum;
 }
 
+/**
+ * @brief Find the max difference between two elements such that the larger element appears after the smaller one.
+ *
+ * Note: we can not sort the array and find the difference between min and max. The order matters.
+ */
 TEST_CASE("find_max_diff", "[array]") {
-    using calculator = function<int(const vector<int> &)>;
-    int idx = 0;
-    for (const auto &fn :
-         {find_max_diff_brutal_force, find_max_diff_optimal,
-          find_max_diff_relative_dist, find_max_diff_relative_dist_optimal}) {
-        INFO("with idx" << idx);
-        CHECK(fn({1, 3, 4, 8, 2, 4, 7}) == 7);
-        CHECK(fn({1, 2, 3, 4, 5, 6, 8}) == 7);
-        CHECK(fn({3, 2, 1}) == 0);
-        CHECK(fn({4, 2, 8, 2, 7, 1, 7}) == 6);
-        CHECK(fn({80, 2, 6, 3, 100}) == 98);
-        idx++;
-    }
+  int idx = 0;
+  for (const auto& fn : {find_max_diff_brutal_force, find_max_diff_optimal, find_max_diff_relative_dist,
+                         find_max_diff_relative_dist_optimal}) {
+    INFO("with idx" << idx);
+    CHECK(fn({1, 3, 4, 8, 2, 4, 7}) == 7);
+    CHECK(fn({1, 2, 3, 4, 5, 6, 8}) == 7);
+    CHECK(fn({3, 2, 1}) == 0);
+    CHECK(fn({4, 2, 8, 2, 7, 1, 7}) == 6);
+    CHECK(fn({80, 2, 6, 3, 100}) == 98);
+    idx++;
+  }
 }
--- algorithms/array/find_missing_numbers.cpp
+++ algorithms/array/find_missing_numbers.cpp
@@ -1,145 +1,146 @@
-#include "algorithms.hpp"
 #include <algorithm>
 #include <initializer_list>
 #include <iostream>
 #include <string>
 #include <vector>
 
+#include "algorithms.hpp"
+
 //#define CHECK(e) if (!(e)) std::cout << "Failure: '" << #e << "'  ==
 // false" << std::endl
-void print_numbers(const std::vector<int> &numbers) {
-    for (int i = 0; i < numbers.size(); ++i) {
-        if (i == 0)
-            std::cout << "Result:[";
-        else
-            std::cout << ", ";
-        std::cout << numbers[i];
-        if (i == numbers.size() - 1)
-            std::cout << "]" << std::endl;
-    }
+void print_numbers(const std::vector<int>& numbers) {
+  for (int i = 0; i < numbers.size(); ++i) {
+    if (i == 0)
+      std::cout << "Result:[";
+    else
+      std::cout << ", ";
+    std::cout << numbers[i];
+    if (i == numbers.size() - 1)
+      std::cout << "]" << std::endl;
+  }
 }
 
-// search the first element
+// search the first element via divide-and-conquer.
 // include left and right
 // case 1: arr = [1, 2, 3, 8, 9], l = r = 2, l = 0, r = 3, l = 0 r = 4
 // case 2: arr = [1, 3, 8, 9], l = r = 2, l = 1, r = 4
-int binary_search(const std::vector<int> &arr, int l, int r, int val) {
-    if (l <= r) {
-        int mid = l + (r - l) / 2;
-        if (arr[mid] == val) {
-            // mid is what we want, but we should move back to skip duplication
-            while (mid - 1 >= l && arr[mid - 1] == val)
-                --mid;
-            return mid;
-        } else if (arr[mid] > val) {
-            return binary_search(arr, l, mid - 1, val);
-        } else {
-            return binary_search(arr, mid + 1, r, val);
-        }
+int binary_search(const std::vector<int>& arr, int l, int r, int val) {
+  if (l <= r) {
+    int mid = l + (r - l) / 2;
+    if (arr[mid] == val) {
+      // mid is what we want, but we should move back to skip duplication
+      while (mid - 1 >= l && arr[mid - 1] == val)
+        --mid;
+      return mid;
+    } else if (arr[mid] > val) {
+      return binary_search(arr, l, mid - 1, val);
+    } else {
+      return binary_search(arr, mid + 1, r, val);
     }
-    return -1;
+  }
+  return -1;
 }
 
-// search the first element
-int binary_search_iterative(const std::vector<int> &arr, int l, int r,
-                            int val) {
-    while (l <= r) {
-        int mid = l + (r - l) / 2;
-        if (arr[mid] == val) {
-            while (mid - 1 >= l && arr[mid - 1] == val)
-                mid--;
-            return mid;
-        } else if (arr[mid] > val) {
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
+// search the first element iteratively.
+int binary_search_iterative(const std::vector<int>& arr, int l, int r, int val) {
+  while (l <= r) {
+    int mid = l + (r - l) / 2;
+    if (arr[mid] == val) {
+      while (mid - 1 >= l && arr[mid - 1] == val)
+        mid--;
+      return mid;
+    } else if (arr[mid] > val) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
     }
-    return -1;
+  }
+  return -1;
 }
 
-std::vector<int> find_missing_numbers(const std::vector<int> &arr, int x,
-                                      int y) {
-    if (x >= y)
-        return {};
+std::vector<int> find_missing_numbers(const std::vector<int>& arr, int x, int y) {
+  if (x >= y)
+    return {};
 
-    const int total = static_cast<int>(arr.size());
-    int b = binary_search(arr, 0, total, x);
-    if (b == -1)
-        return {};
-    int e = binary_search(arr, b, total, y);
-    if (e == -1)
-        return {};
+  const int total = static_cast<int>(arr.size());
+  int b = binary_search(arr, 0, total, x);
+  if (b == -1)
+    return {};
+  int e = binary_search(arr, b, total, y);
+  if (e == -1)
+    return {};
 
-    auto move_next = [&](int idx) {
-        while (++idx < total && arr[idx] == arr[idx - 1])
-            ;
-        return idx;
-    };
-
-    std::vector<int> results;
-    b = move_next(b);
-    for (int v = x + 1; v < y && b < total; ++v) {
-        if (v == arr[b]) {
-            b = move_next(b);
-        } else {
-            results.push_back(v);
-        }
+  auto move_next = [&](int idx) {
+    while (++idx < total && arr[idx] == arr[idx - 1]) {
     }
-    return results;
+    return idx;
+  };
+
+  std::vector<int> results;
+  b = move_next(b);
+  for (int v = x + 1; v < y && b < total; ++v) {
+    if (v == arr[b]) {
+      b = move_next(b);
+    } else {
+      results.push_back(v);
+    }
+  }
+  return results;
 }
 
-std::vector<int> find_missing_numbers_2(const std::vector<int> &sorted_numbers,
-                                        int x, int y) {
-    if (x == y) {
-        return {};
+std::vector<int> find_missing_numbers_2(const std::vector<int>& sorted_numbers, int x, int y) {
+  if (x == y) {
+    return {};
+  }
+  auto it_x = std::find(std::begin(sorted_numbers), std::end(sorted_numbers), x);
+  if (it_x == std::end(sorted_numbers)) {
+    return {};
+  }
+
+  auto it_y = std::find(std::begin(sorted_numbers), std::end(sorted_numbers), y);
+  if (it_y == std::end(sorted_numbers)) {
+    return {};
+  }
+
+  while (std::next(it_x) != std::end(sorted_numbers) && *std::next(it_x) == x)
+    ++it_x;
+
+  std::vector<int> numbers;
+  auto it = std::next(it_x);
+  for (int i = x + 1; i < y; ++i) {
+    if (i == *it) {
+      ++it;
+      while (i == *it)
+        ++it;
+
+    } else {
+      numbers.push_back(i);
     }
-    auto it_x =
-        std::find(std::begin(sorted_numbers), std::end(sorted_numbers), x);
-    if (it_x == std::end(sorted_numbers)) {
-        return {};
-    }
+  }
 
-    auto it_y =
-        std::find(std::begin(sorted_numbers), std::end(sorted_numbers), y);
-    if (it_y == std::end(sorted_numbers)) {
-        return {};
-    }
-
-    while (std::next(it_x) != std::end(sorted_numbers) && *std::next(it_x) == x)
-        ++it_x;
-
-    std::vector<int> numbers;
-    auto it = std::next(it_x);
-    for (int i = x + 1; i < y; ++i) {
-        if (i == *it) {
-            ++it;
-            while (i == *it)
-                ++it;
-
-        } else {
-            numbers.push_back(i);
-        }
-    }
-
-    print_numbers(sorted_numbers);
-    return numbers;
+  print_numbers(sorted_numbers);
+  return numbers;
 }
 
+/**
+ * @brief Find the missing elements between two given numbers from a sequence.
+ *
+ * 1. Write a simple binary search in divide-and-conquer and iterative styles.
+ *    This will shrink the search range.
+ * 2. Find the missing number and skip the duplicated ones.
+ *
+ */
 TEST_CASE("find_missing_numbers", "[array]") {
-    CHECK(binary_search_iterative({1, 3, 7, 9, 17, 23}, 1, 4, 9) == 3);
-    CHECK(binary_search_iterative({1, 3, 3, 7, 9, 9, 17, 23}, 0, 6, 3) == 1);
-    CHECK(binary_search_iterative({1, 3, 3, 7, 7, 9, 9, 17, 20}, 1, 7, 9) == 5);
+  CHECK(binary_search_iterative({1, 3, 7, 9, 17, 23}, 1, 4, 9) == 3);
+  CHECK(binary_search_iterative({1, 3, 3, 7, 9, 9, 17, 23}, 0, 6, 3) == 1);
+  CHECK(binary_search_iterative({1, 3, 3, 7, 7, 9, 9, 17, 20}, 1, 7, 9) == 5);
 
-    CHECK(binary_search({1, 3, 7, 9, 17, 23}, 1, 4, 9) == 3);
-    CHECK(binary_search({1, 3, 3, 7, 9, 9, 17, 23}, 0, 6, 3) == 1);
-    CHECK(binary_search({1, 3, 3, 7, 7, 9, 9, 17, 20}, 1, 7, 9) == 5);
+  CHECK(binary_search({1, 3, 7, 9, 17, 23}, 1, 4, 9) == 3);
+  CHECK(binary_search({1, 3, 3, 7, 9, 9, 17, 23}, 0, 6, 3) == 1);
+  CHECK(binary_search({1, 3, 3, 7, 7, 9, 9, 17, 20}, 1, 7, 9) == 5);
 
-    CHECK(find_missing_numbers({1, 3, 7, 9, 17, 23}, 3, 9) ==
-          std::vector<int>({4, 5, 6, 8}));
-    CHECK(find_missing_numbers({1, 3, 3, 7, 9, 9, 17, 23}, 3, 9) ==
-          std::vector<int>({4, 5, 6, 8}));
-    CHECK(
-        find_missing_numbers({1, 3, 3, 7, 7, 9, 9, 17, 20}, 1, 20) ==
+  CHECK(find_missing_numbers({1, 3, 7, 9, 17, 23}, 3, 9) == std::vector<int>({4, 5, 6, 8}));
+  CHECK(find_missing_numbers({1, 3, 3, 7, 9, 9, 17, 23}, 3, 9) == std::vector<int>({4, 5, 6, 8}));
+  CHECK(find_missing_numbers({1, 3, 3, 7, 7, 9, 9, 17, 20}, 1, 20) ==
         std::vector<int>({2, 4, 5, 6, 8, 10, 11, 12, 13, 14, 15, 16, 18, 19}));
 }
