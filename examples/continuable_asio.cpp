1e7d48eb5e6e7c343ddc1faecf037604a98aa2bc
Weidong Lian <weidong.lian@gmail.com>
2021-04-30 14:08:06 +0200

    add error handling in continuable

--- examples/continuable_asio.cpp
+++ examples/continuable_asio.cpp
@@ -16,7 +16,11 @@ auto calc_square_async(float val, asio::thread_pool* pool) {
     asio::post(*pool, [val, promise = std::forward<decltype(promise)>(promise)]() mutable {
       std::cout << "start async resolver\n";
       std::this_thread::sleep_for(std::chrono::seconds(5));
-      promise.set_value(val * val);
+      if (val < 0) {
+        promise.set_exception(std::make_error_condition(std::errc::invalid_argument));
+      } else {
+        promise.set_value(val * val);
+      }
       std::cout << "end async resolver\n";
     });
   });
@@ -43,6 +47,20 @@ int main(int argc, char** argv) {
     std::cout << "calc_square is resolved with:" << result << " thread_id:" << std::this_thread::get_id() << std::endl;
   });
   std::cout << "calc_square_async end\n";
+
+  // calc square async continuable with failure
+  std::cout << "calc_square_async fail begin\n";
+  calc_square_async(-5.0f, &pool)
+    .then([](float result) {
+      std::cout << "calc_square async fail is resolved with:" << result << " thread_id:" << std::this_thread::get_id()
+                << std::endl;
+    })
+    .fail([](cti::exception_t e) {
+      std::cout << "calc_square async fail is rejected with:" << e.value() << "," << e.message()
+                << " thread_id:" << std::this_thread::get_id() << std::endl;
+    });
+  std::cout << "calc_square_async fail end\n";
+
   pool.join();
   return 0;
 }
