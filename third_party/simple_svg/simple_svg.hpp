a08e6858e112eb70e6ee69e0a095bf813d18bbe6
Weidong Lian <weidong.lian@gmail.com>
2023-01-07 23:57:28 +0100

    use std optional in simple svg

--- third_party/simple_svg/simple_svg.hpp
+++ third_party/simple_svg/simple_svg.hpp
@@ -63,30 +63,6 @@ namespace svg
         return "/>\n";
     }
 
-    // Quick optional return type.  This allows functions to return an invalid
-    //  value if no good return is possible.  The user checks for validity
-    //  before using the returned value.
-    template <typename T>
-    class optional
-    {
-    public:
-        optional<T>(T const & type)
-            : valid(true), type(type) { }
-        optional<T>() : valid(false), type(T()) { }
-        T * operator->()
-        {
-            // If we try to access an invalid value, an exception is thrown.
-            if (!valid)
-                throw std::exception();
-
-            return &type;
-        }
-        // Test for validity.
-        bool operator!() const { return !valid; }
-    private:
-        bool valid;
-        T type;
-    };
 
     struct Dimensions
     {
@@ -102,10 +78,10 @@ namespace svg
         double x;
         double y;
     };
-    inline optional<Point> getMinPoint(std::vector<Point> const & points)
+    inline std::optional<Point> getMinPoint(std::vector<Point> const & points)
     {
         if (points.empty())
-            return optional<Point>();
+            return std::optional<Point>();
 
         Point min = points[0];
         for (unsigned i = 0; i < points.size(); ++i) {
@@ -114,12 +90,12 @@ namespace svg
             if (points[i].y < min.y)
                 min.y = points[i].y;
         }
-        return optional<Point>(min);
+        return std::optional<Point>(min);
     }
-    inline optional<Point> getMaxPoint(std::vector<Point> const & points)
+    inline std::optional<Point> getMaxPoint(std::vector<Point> const & points)
     {
         if (points.empty())
-            return optional<Point>();
+            return std::optional<Point>();
 
         Point max = points[0];
         for (unsigned i = 0; i < points.size(); ++i) {
@@ -128,7 +104,7 @@ namespace svg
             if (points[i].y > max.y)
                 max.y = points[i].y;
         }
-        return optional<Point>(max);
+        return std::optional<Point>(max);
     }
 
     // Defines the dimensions, scale, origin, and origin offset of the document.
@@ -454,9 +430,9 @@ namespace svg
     {
     public:
        Path(Fill const & fill = Fill(), Stroke const & stroke = Stroke())
-          : Shape(fill, stroke) 
+          : Shape(fill, stroke)
        {  startNewSubPath(); }
-       Path(Stroke const & stroke = Stroke()) : Shape(Color::Transparent, stroke) 
+       Path(Stroke const & stroke = Stroke()) : Shape(Color::Transparent, stroke)
        {  startNewSubPath(); }
        Path & operator<<(Point const & point)
        {
@@ -606,13 +582,13 @@ namespace svg
         double scale;
         std::vector<Polyline> polylines;
 
-        optional<Dimensions> getDimensions() const
+        std::optional<Dimensions> getDimensions() const
         {
             if (polylines.empty())
-                return optional<Dimensions>();
+                return std::optional<Dimensions>();
 
-            optional<Point> min = getMinPoint(polylines[0].points);
-            optional<Point> max = getMaxPoint(polylines[0].points);
+            std::optional<Point> min = getMinPoint(polylines[0].points);
+            std::optional<Point> max = getMaxPoint(polylines[0].points);
             for (unsigned i = 0; i < polylines.size(); ++i) {
                 if (getMinPoint(polylines[i].points)->x < min->x)
                     min->x = getMinPoint(polylines[i].points)->x;
@@ -624,11 +600,11 @@ namespace svg
                     max->y = getMaxPoint(polylines[i].points)->y;
             }
 
-            return optional<Dimensions>(Dimensions(max->x - min->x, max->y - min->y));
+            return std::optional<Dimensions>(Dimensions(max->x - min->x, max->y - min->y));
         }
         std::string axisString(Layout const & layout) const
         {
-            optional<Dimensions> dimensions = getDimensions();
+            std::optional<Dimensions> dimensions = getDimensions();
             if (!dimensions)
                 return "";
 
